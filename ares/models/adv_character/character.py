721bd50d7fdac651c4f03ae825dd116e6b59a673
Sean Toner <placeoftheway@gmail.com>
2023-09-25 02:59:01 -0400

    Removing advanced for now

--- ares/models/adv_character/character.py
+++
@@ -1,9 +0,0 @@
-
-from abc import ABC
-from typing import Literal, TypeAlias
-from pydantic import BaseModel
-
-from ares.models.characteristic import Attribute
-
-class Character(BaseModel):
-    attributes: list[Attribute]
